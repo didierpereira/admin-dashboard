35bd6b6e0eedf536477de2110f528dfb796fd6dc
Didier Pereira <krrattoss5@gmail.com>
2026-05-02 20:19:37 -0500

    style: hide scrollbars on all dashboard pages
    - Apply scrollbar-hide class to main containers across all pages

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -9,7 +9,7 @@ import SalesChannelChart from "../components/overview/SalesChannelChart"
 
 const OverviewPage = () => {
   return (
-    <div className="flex-1 overflow-auto relative z-10">
+    <div className="flex-1 overflow-auto relative z-10 scrollbar-hide">
       <Header title="Overview" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
